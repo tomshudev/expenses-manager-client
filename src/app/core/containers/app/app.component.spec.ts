86005923ee0dcdfe12f79e4d07638a142a82b91c
tomshudev <tomshudev@gmail.com>
2019-12-09 22:03:05 +0200

    Fix tests for the ci

--- src/app/core/containers/app/app.component.spec.ts
+++ src/app/core/containers/app/app.component.spec.ts
@@ -2,12 +2,24 @@ import { TestBed, async } from "@angular/core/testing";
 import { AppComponent } from "./app.component";
 import { SidebarComponent } from "../../components/sidebar/sidebar.component";
 import { SharedModule } from "src/app/shared/shared.module";
+import { RouterModule } from "@angular/router";
+import { StoreModule } from "@ngrx/store";
+import { coreReducer } from "../../reducers/core.reducer";
+import { appRoutes } from "../../routing/routes";
+import { MaxExpenseModule } from "src/app/features/max-expense/max-expense.module";
+import { ExpensesService } from "../../services/expenses.service";
 
 describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent, SidebarComponent],
-      imports: [SharedModule]
+      providers: [ExpensesService],
+      imports: [
+        MaxExpenseModule,
+        SharedModule,
+        RouterModule.forRoot(appRoutes),
+        StoreModule.forRoot({ core: coreReducer })
+      ]
     }).compileComponents();
   }));
 
--- src/app/core/services/expenses.service.spec.ts
+++ src/app/core/services/expenses.service.spec.ts
@@ -1,11 +1,17 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { ExpensesService } from './expenses.service';
+import { ExpensesService } from "./expenses.service";
+import { StoreModule } from "@ngrx/store";
+import { coreReducer } from "../reducers/core.reducer";
 
-describe('ExpensesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe("ExpensesService", () => {
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [StoreModule.forRoot({ core: coreReducer })]
+    })
+  );
 
-  it('should be created', () => {
+  it("should be created", () => {
     const service: ExpensesService = TestBed.get(ExpensesService);
     expect(service).toBeTruthy();
   });
