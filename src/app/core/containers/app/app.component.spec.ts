075586275ea7cc4ace3aed81f1364aa6f1f30a48
tomshudev <tomshudev@gmail.com>
2019-12-09 21:41:16 +0200

    Fixed failing tests

--- src/app/core/containers/app/app.component.spec.ts
+++ src/app/core/containers/app/app.component.spec.ts
@@ -1,31 +1,19 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+import { SidebarComponent } from "../../components/sidebar/sidebar.component";
+import { SharedModule } from "src/app/shared/shared.module";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent, SidebarComponent],
+      imports: [SharedModule]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'expenses-manager-client'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('expenses-manager-client');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('expenses-manager-client app is running!');
   });
 });
