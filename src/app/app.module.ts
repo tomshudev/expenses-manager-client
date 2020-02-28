0429f2622508eef6cb20fd482f5f77cf760e9f10
tomshudev <tomshudev@gmail.com>
2020-02-28 10:54:22 +0200

    Finish first design of the max-expense

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,18 @@ import { CoreModule } from "./core/core.module";
 import { RoutingModule } from "./core/routing/routing.module";
 import { RouterModule } from "@angular/router";
 import { appRoutes } from "./core/routing/routes";
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes), BrowserModule, CoreModule],
-  providers: [],
+  imports: [RouterModule.forRoot(appRoutes), BrowserModule, CoreModule, NgZorroAntdModule, FormsModule, HttpClientModule, BrowserAnimationsModule],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
